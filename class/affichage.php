0cc49ca7f53385f5e2dd5b42ddee0260e1bb0ed2
gwenael-mahe <gwenael.mahe@laplateforme.io>
2020-02-28 14:01:28 +0100

    affichage.php

--- class/affichage.php
+++ class/affichage.php
@@ -4,7 +4,16 @@ include 'bdd.php'
 
 class affichage{
 
-    
+    private $bdd;
+
+    public function __construct()
+    {
+        $bdd = new bdd();
+        $this->bdd = $bdd->getco();
+    }
+    public function product($id){
+        $request = ""
+    }
 }
 
 ?>
