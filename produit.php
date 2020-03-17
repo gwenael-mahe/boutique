32f6c74963cf7e8dd2e45c842627b87e79de133d
gwénaël <gwenael.mahe@laplateforme.io>
2020-03-17 10:27:25 +0100

    modif page produit + profil

--- produit.php
+++ produit.php
@@ -18,7 +18,19 @@ if(isset($_POST['addtocart'])){
 if(isset($_POST['send'])){
     $affichage->addcommentaire($_SESSION['id'],$_POST["com"],$_GET['id'],$_POST['note']);
 }
+if (isset($_POST['modifierproduit'])) {
+    $affichage->get('admin')->majproduit($_POST['nom'], $_POST['prix'], addslashes($_POST['descriptionup']), addslashes($_POST['descriptiondown']), $_POST['sous_categorie'], $_POST['id']);
+    header('location:produit.php');
+}
 
+if (isset($_POST['ajouterproduit'])) {
+    $affichage->get('admin')->ajoutproduit($_POST['nom'], $_POST['prix'], addslashes($_POST['descriptionup']), addslashes($_POST['descriptiondown']), $_POST['sous_categorie']);
+    header('location:produit.php');
+}
+
+if (isset($_GET['recherche'])) {
+    header('location:boutique.php?recherche=' . $_GET['recherche']);
+}
 ?>
 
 <!DOCTYPE html>
@@ -35,12 +47,30 @@ if(isset($_POST['send'])){
 
     <main id="mainproduit">
     <?php
-    $affichage->product($_GET['id']);
-    $affichage->notation($_GET['id']);
-    $affichage->commentaire($_GET['id']);
-    if(isset($_SESSION['login'])){
-        $affichage->formmessage();
+    if (isset($_SESSION['login']) && $_SESSION['login'] == 'admin' && (isset(explode('?', $_SERVER['REQUEST_URI'])[1]) == false)) { ?>
+
+        <h1> Gestion des produits </h1>
+        
+        <section class='categorie'>
+        
+            <?php $affichage->adminproduit_modif($affichage->get('admin')->get('bdd')->get('produitadmin'), $affichage->get('admin')->get('bdd')->get('souscategorie'));
+            $affichage->adminproduit_ajout($affichage->get('admin')->get('bdd')->get('souscategorie')); ?>
+        
+        </section>
+        
+        <?php } else { ?>
+        
+        
+        <?php } 
+    if(isset(explode('?', $_SERVER['REQUEST_URI'])[1]) == true){
+        $affichage->product($_GET['id']);
+        $affichage->notation($_GET['id']);
+        $affichage->commentaire($_GET['id']);
+        if(isset($_SESSION['login'])){
+            $affichage->formmessage();
+        }
     }
+    
     ?>
     </main>
 
